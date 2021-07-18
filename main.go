004ab882de901adc832d17eea0967f21cafe355e
zhaozhe <zzzlx@live.com>
2021-07-18 18:44:21 +0800

    upload page

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("html/*")
+	r.StaticFS("/static", http.Dir("./static"))
 
 	r.GET("/", index)
+	r.GET("/upload", upload)
 
 	r.Run(config.GlobalConfig.Port)
 }
@@ -19,4 +21,8 @@ func main() {
 // 返回静态页
 func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
+}
+
+func upload(c *gin.Context) {
+	c.HTML(http.StatusOK, "upload.html", nil)
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
+swagger generate spec -o ./swagger.json
+
+swagger serve swagger.json
+
+swagger serve -F=swagger swagger.json
+
 https://github.com/gin-gonic/gin
 
 https://github.com/go-yaml/yaml
